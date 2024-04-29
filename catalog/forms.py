aaefcdb0baa5994411bb367da8063adfc3201522
Sergey Shiryaev <shiryaev_fox@mail.ru>
2024-04-29 17:41:19 +0300

    Task 3 completed part 2

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,10 +1,21 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.forms import BooleanField
 
 from catalog.models import Product, Version
 
 
-class ProductForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Product
         fields = ('name', 'discription', 'image', 'category', 'price',)
@@ -27,7 +38,7 @@ class ProductForm(forms.ModelForm):
         return discription
 
 
-class VersionForm(forms.ModelForm):
+class VersionForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Version
         fields = '__all__'
