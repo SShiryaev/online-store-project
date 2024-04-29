4a6d8eb7950c70e816cb896c95055afd848f7c47
Sergey Shiryaev <shiryaev_fox@mail.ru>
2024-04-29 17:41:10 +0300

    Task 3 completed

--- catalog/views.py
+++ catalog/views.py
@@ -50,15 +50,15 @@ class ProductUpdateView(UpdateView):
         context_data = super().get_context_data(**kwargs)
         VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
         if self.request.method == 'POST':
-            context_data['formset'] = VersionFormset(self.request.POST)
+            context_data['formset'] = VersionFormset(self.request.POST, instance=self.object)
         else:
-            context_data['formset'] = VersionFormset()
+            context_data['formset'] = VersionFormset(instance=self.object)
         return context_data
 
     def form_valid(self, form):
         formset = self.get_context_data()['formset']
         self.object = form.save()
-        if formset.is_valid():
+        if form.is_valid() and formset.is_valid():
             formset.instance = self.object
             formset.save()
         return super().form_valid(form)
