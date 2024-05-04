a338e432348c81e7b076391c7a0332380168a1f7
Sergey Shiryaev <shiryaev_fox@mail.ru>
2024-05-04 07:13:48 +0300

    Task 2

--- users/models.py
+++ users/models.py
@@ -12,6 +12,8 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to='users/', **NULLABLE, verbose_name='аватар')
     country = models.CharField(max_length=80, **NULLABLE, verbose_name='страна')
 
+    token = models.CharField(max_length=100, **NULLABLE, verbose_name='токен')
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
