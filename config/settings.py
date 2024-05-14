aeefdf8207820dc50fbb922d0c954f6e0cf10b43
Sergey Shiryaev <shiryaev_fox@mail.ru>
2024-05-15 01:06:15 +0300

    HW 23.2 init

--- config/settings.py
+++ config/settings.py
@@ -156,3 +156,10 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = EMAIL_HOST_USER
 EMAIL_ADMIN = EMAIL_HOST_USER
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
