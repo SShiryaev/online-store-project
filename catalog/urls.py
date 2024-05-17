16a6f3da8ca6884a0efc8ae8b25af81851467449
Sergey Shiryaev <shiryaev_fox@mail.ru>
2024-05-17 07:17:51 +0300

    HW 23.2 task 2, 3

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from catalog.apps import CatalogConfig
 from catalog.views import (ProductListView, ProductDetailView, ProductUpdateView,
@@ -10,7 +11,7 @@ urlpatterns = [
     path('create/', ProductCreateView.as_view(), name="create_product"),
     path('', ProductListView.as_view(), name='list_product'),
     path('contacts/', FeedbackCreateView.as_view(), name='contacts'),
-    path('view/<int:pk>/', ProductDetailView.as_view(), name="view_product"),
+    path('view/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name="view_product"),
     path('edit/<int:pk>/', ProductUpdateView.as_view(), name="edit_product"),
     path('delete/<int:pk>/', ProductDeleteView.as_view(), name="delete_product"),
     path('stock/<int:pk>/', toggle_stock, name="toggle_stock"),
